6615a6c35d0d2e5cd2827030177b6c5cfc9f6ae3
naveenkumar6788 <naveenkumardasari6788@gmail.com>
2026-06-18 21:51:55 +0530

    fac

---
+++ fac_rec.java
@@ -0,0 +1,12 @@
+public class fac_rec {
+    public static void main(String[] args) {
+        int ans=fac(5);
+        System.out.println(ans);
+    }
+    static int fac(int n){
+        if(n<=1){
+            return 1;
+        }
+        return n+fac(n-1);
+    }
+}
